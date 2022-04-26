7a9e1a945982c1d71dae85aeee768b5090a3157d
Ayush Gupta <ayushgupta747492@gmail.com>
2022-04-26 12:16:16 +0530

    Add files via upload

---
+++ R/Sum.R
@@ -0,0 +1,12 @@
+
+
+#'This function is used to add 2 numbers
+#'@export
+#'@param x numeric variable
+#'@param y numeric variable
+#'@return result numeric variable
+
+addSum <- function(x,y){
+  result <- x+y
+  return(result)
+}
